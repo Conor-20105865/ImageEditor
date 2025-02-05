c83098965ee283cde6760b422b3b8ac83d54a84b
Conor-20105865 <147147702+Conor-20105865@users.noreply.github.com>
2025-02-05 14:34:32 +0000

    Fixed RGB sliders

--- src/main/java/com/example/imageeditor/ImageEditorController.java
+++ src/main/java/com/example/imageeditor/ImageEditorController.java
@@ -2,7 +2,7 @@ package com.example.imageeditor;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
@@ -11,7 +11,6 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
 import java.io.File;
 
 public class ImageEditorController {
@@ -20,8 +19,17 @@ public class ImageEditorController {
     private ImageView imageView;
 
     @FXML
-    private CheckBox greyscaleCheckBox; // Link to FXML
-    //stores og image
+    private CheckBox greyscaleCheckBox;
+
+    @FXML
+    private Slider redSlider;
+
+    @FXML
+    private Slider greenSlider;
+
+    @FXML
+    private Slider blueSlider;
+
     private Image originalImage;
 
     @FXML
@@ -36,33 +44,25 @@ public class ImageEditorController {
                 new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif", "*.bmp")
         );
 
-        Stage stage = new Stage();
+        Stage stage = (Stage) imageView.getScene().getWindow();
         File file = fileChooser.showOpenDialog(stage);
 
         if (file != null) {
             originalImage = new Image(file.toURI().toString());
             imageView.setImage(originalImage);
-
-            // Reset the checkbox state when a new image is loaded
-            greyscaleCheckBox.setSelected(false);
+            greyscaleCheckBox.setSelected(false); // Reset greyscale checkbox
         }
     }
 
     @FXML
     public void handleGreyscaleToggle() {
         if (originalImage == null) return;
-
-        if (greyscaleCheckBox.isSelected()) {
-            imageView.setImage(applyGreyscale(originalImage));
-        } else {
-            imageView.setImage(originalImage);
-        }
+        imageView.setImage(greyscaleCheckBox.isSelected() ? applyGreyscale(originalImage) : originalImage);
     }
 
     private Image applyGreyscale(Image image) {
         int width = (int) image.getWidth();
         int height = (int) image.getHeight();
-
         WritableImage greyscaleImage = new WritableImage(width, height);
         PixelReader pixelReader = image.getPixelReader();
         PixelWriter pixelWriter = greyscaleImage.getPixelWriter();
@@ -75,7 +75,33 @@ public class ImageEditorController {
                 pixelWriter.setColor(x, y, greyColor);
             }
         }
-
         return greyscaleImage;
+    }
+
+    @FXML
+    private void changeColorChannels() {
+        if (originalImage == null) return;
+        int width = (int) originalImage.getWidth();
+        int height = (int) originalImage.getHeight();
+        WritableImage adjustedImage = new WritableImage(width, height);
+        PixelReader pixelReader = originalImage.getPixelReader();
+        PixelWriter pixelWriter = adjustedImage.getPixelWriter();
+
+        double redScale = redSlider.getValue();
+        double greenScale = greenSlider.getValue();
+        double blueScale = blueSlider.getValue();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color color = pixelReader.getColor(x, y);
+                double newRed = Math.min(color.getRed() * redScale, 1.0);
+                double newGreen = Math.min(color.getGreen() * greenScale, 1.0);
+                double newBlue = Math.min(color.getBlue() * blueScale, 1.0);
+                Color newColor = new Color(newRed, newGreen, newBlue, color.getOpacity());
+                pixelWriter.setColor(x, y, newColor);
+            }
+        }
+
+        imageView.setImage(adjustedImage);
     }
 }
